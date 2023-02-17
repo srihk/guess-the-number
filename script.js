c56585dffa93b2c541840a2318303eb4d1d6d811
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2023-02-17 15:38:36 +0530

    Add sources.

---
+++ script.js
@@ -0,0 +1,61 @@
+const key = Math.floor(Math.random() * 100 + 1);
+
+const lastResult = document.querySelector('.lastResult');
+const guessField = document.querySelector('.guessField');
+const lowOrHi = document.querySelector('.lowOrHi');
+const guesses = document.querySelector('.guesses');
+const guessSubmit = document.querySelector('.guessSubmit');
+const resultParas = document.querySelector('.resultParas');
+const form = document.querySelector('form');
+let restartButton = document.createElement('button');
+restartButton.textContent = 'Restart';
+
+let attempt = 0;
+
+restartButton.addEventListener('click', () => {
+    guessField.value = '';
+    location.reload();
+});
+
+function end() {
+    resultParas.appendChild(restartButton);
+    guessField.setAttribute('disabled', true);
+    guessSubmit.setAttribute('disabled', true);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (attempt == 10) {
+        lastResult.textContent = "Sorry, You are locked!";
+        end();
+        return;
+    }
+
+    if (guesses.textContent.length == 0) {
+        guesses.append(`Previous Guesses: `);
+    }
+
+    ++attempt;
+    const guess = guessField.value;
+
+    if (guess == key) {
+        guesses.append(` ${guess}`);
+        lastResult.textContent = `Congrats! The key is ${key}. You are free.`;
+        lastResult.style.background = "rgba(0, 200, 0, 0.2)";
+        lowOrHi.textContent = '';
+        end();
+        return;
+    }
+
+    guesses.append(` ${guess}`);
+    lowOrHi.style.background = "rgba(200, 0, 0, 0.2)";
+    if (guess < key) {
+        lowOrHi.textContent = `The key is greater than ${guess}.`;
+    }
+    else {
+        lowOrHi.textContent = `The key is less than ${guess}.`;
+    }
+
+    guessField.value = '';
+    guessField.focus();
+});
